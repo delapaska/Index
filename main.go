532a4e06ba98608a3ea23df81229e772f2e0e784
delapaska <37419828+delapaska@users.noreply.github.com>
2023-09-26 19:48:12 +0300

    Rabin-Karp_upd

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 	"unicode/utf8"
 )
@@ -52,31 +51,33 @@ func IndexRabinKarp(s, substr string) int {
 	var a rune
 
 	var size int
-	k := 0
-	var ans []rune
-	i := 0
-	for k < len(s) {
-		a, size = utf8.DecodeRuneInString(s[k:])
-		fmt.Println(string(a))
-		if i < n {
+	str_index := 0
+
+	counter := 0
+	substr_index := 0
+	for str_index < len(s) {
+		a, size = utf8.DecodeRuneInString(s[str_index:])
+
+		if counter < n {
 			h1 = h1*PrimeRK1 + uint32(a)
 			h2 = h2*PrimeRK2 + uint32(a)
-			ans = append(ans, a)
+
 		} else {
+
+			b, size1 := utf8.DecodeRuneInString(s[substr_index:])
+
 			h1 *= PrimeRK1
 			h2 *= PrimeRK2
-			h1 += uint32(a) - pow1*uint32(ans[0])
-			h2 += uint32(a) - pow2*uint32(ans[0])
+			h1 += uint32(a) - pow1*uint32(b)
+			h2 += uint32(a) - pow2*uint32(b)
 
-			ans = append(ans[:0], ans[1:]...)
-			ans = append(ans, a)
+			substr_index += size1
 		}
-		i++
-		k += size
+		counter++
+		str_index += size
 
-		if h1 == hash1 && h2 == hash2 && string(ans) == substr {
-
-			return i - n
+		if h1 == hash1 && h2 == hash2 && s[str_index-len(substr):str_index] == substr {
+			return counter - n
 		}
 	}
 
